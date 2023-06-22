b2b5422f59dfa38dfffd3943c5aedb20ba80e19f
LanceFontanilla <lancefontanilla@gmail.com>
2023-06-21 22:22:28 -0600

    practice test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const ageToVote = 18
+const ageToVote = 100
 
 const user = {
   name: 'Sherlock Holmes',
@@ -17,5 +17,40 @@ const cases = [
 ]
 
 // YOUR JS CODE
+let currentYear = 2023
+let jsReleaseYear = 1995
+console.log('current age of JavaScript is', currentYear - jsReleaseYear)
 
 
+if (currentYear-jsReleaseYear >= ageToVote){
+  console.log('JavaScript is old enough to vote')
+  }
+  else {
+    console.log('JavaScript is not old enough to vote')
+  }
+
+  console.log ('Starting')
+for (let i = 0; i <= 100; i++){
+  
+  if(i % 10 == 0){
+  console.log (i)
+  }
+  if(i == 50){
+    console.log ('Halfway')
+  }
+}
+console.log ('Finished')
+
+console.log (user.name + ' lives at '+ user.address.street +', '+ user.address.city)
+
+for (let i = 0; i < cases.length; i++) {
+  let xCase = cases[i]
+  console.log (xCase.title, xCase.releaseYear)
+}
+
+for (let i = 0; i < cases.length; i++){
+  let xCase = cases[i]
+  if (xCase.releaseYear == 1903){
+    console.log(xCase.title)
+  }
+}
